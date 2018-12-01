4fa8301e84f2829dbcb49299e10d51e131f96dfe
Kemal Fadillah <fadillzzz@gmail.com>
2018-12-01 05:57:21 -0800

    Updates reducer for registration (or auth) results

--- src/reducers/Register.js
+++ src/reducers/Register.js
@@ -1,15 +1,16 @@
 import {REGISTERING, REGISTER_SUCCESS, REGISTER_FAILURE} from '../actions/Register';
 
 const initialState = {
-    email: '',
-    password: '',
     error: ''
 };
 
 export default function register(state = initialState, action) {
     switch (action.type) {
         case REGISTER_SUCCESS:
-            return Object.assign({}, initialState);
+            localStorage.setItem('authToken', action.token);
+            return {...initialState};
+        case REGISTER_FAILURE:
+            return {error: action.error};
         default:
             return state;
     }
