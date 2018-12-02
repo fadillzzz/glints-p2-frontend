e64bb414bc6eb8b14f0168b195d5646ab4fb271b
Kemal Fadillah <fadillzzz@gmail.com>
2018-12-02 04:44:19 -0800

    Fixes ID attribute

--- src/reducers/Search.js
+++ src/reducers/Search.js
@@ -11,6 +11,7 @@ const initialState = {
 export default function search(state = initialState, action) {
     switch (action.type) {
         case SEARCH_SUCCESS:
+            action.restaurants.forEach(r => r.id = r._id);
             return {...state, restaurants: action.restaurants};
         case SEARCH_FAILURE:
             return {...state, error: action.error};
